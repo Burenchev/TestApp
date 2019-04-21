6ac538f7b264b73decf0d337ce18dbb63947e1d6
Burenchev <49619384+Burenchev@users.noreply.github.com>
2019-04-21 13:39:44 +0500

    Add files via upload

---
+++ build/precache-manifest.1192f0c31081c19c62155b3e86be7e61.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0816bee6c501520e4a74",
+    "url": "/static/js/main.a98d3bc7.chunk.js"
+  },
+  {
+    "revision": "9dcb7f644090a88758a3",
+    "url": "/static/js/2.5e725aa5.chunk.js"
+  },
+  {
+    "revision": "2caac6f2811834683dcc76a05fed6c54",
+    "url": "/index.html"
+  }
+];
